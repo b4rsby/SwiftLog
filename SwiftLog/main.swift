d3d9d34e7f9a8fad66df9e2ec416b85fe60fc40a
AntiHaus <antihaus@AntiHauss-MacBook-Pro.local>
2015-01-15 22:12:20 -0500

    Initial Commit

---
+++ SwiftLog/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLog
+//
+//  Created by AntiHaus on 1/15/15.
+//  Copyright (c) 2015 AntiHaus. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
